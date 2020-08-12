1556722fc6e44c24429e90c5bec38db46c6b3347
Repesco Vadim <repescov@gmail.com>
2020-08-12 20:41:09 +0300

    creare un nou dosar cu tema pentru acasa

---
+++ Lectia5/SalutLume.java
@@ -0,0 +1,19 @@
+// Primul program in java
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Salut, lume buna!");
+    }
+}
+
+class MyNameIs {
+    public static void main(String[] args) {
+        System.out.println("Numele meu este Repesco Vadim");
+    }
+}
+
+class TurnCoffeToJavaCode {
+    public static void main(String[] args) {
+        System.out.println("Transformam cafeaua in cod java");
+    }
+}
