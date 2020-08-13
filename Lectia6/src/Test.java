02ba5beb1bf61d0c794510f82e2b9ddee68c6a09
Repesco Vadim <repescov@gmail.com>
2020-08-13 20:23:51 +0300

    testare tip variabile

--- Lectia6/src/Test.java
+++ Lectia6/src/Test.java
@@ -1,6 +1,14 @@
 public class Test {
     public static void main(String[] params){
         FirstClass primaclasa = new FirstClass();
-        FirstClass primaclasa2 = new FirstClass();
+        byte b = 60;
+        short s = 22;
+        int i = -23000;
+        long l = 10202020;
+        float f = 9.5F;
+        double d = 11.2345;
+        char c = 'B';
+        boolean bool = true;
+        System.out.println("variabila byte b="+b);
     }
 }
