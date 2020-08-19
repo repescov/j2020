962eff02b8b0b2c1befc5606ade2a20460cf512a
Repesco Vadim <repescov@gmail.com>
2020-08-19 18:53:10 +0300

    Conditionale lectia 8

---
+++ Lectia8/Conditionale.java
@@ -0,0 +1,12 @@
+public class Conditionale{
+    public static void main(String[] params){
+        int a1 = 10;
+        int a2 = 12;
+        if (a1<a2) {
+            System.out.println("Numarul "+a1+" este mai mic decat "+a2);
+        }
+
+    }
+
+
+}
