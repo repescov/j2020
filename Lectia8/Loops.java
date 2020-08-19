b301fe09237b8358ef4bb2d5d85dd8f332ba57a0
Repesco Vadim <repescov@gmail.com>
2020-08-19 20:38:29 +0300

    Conditionale lectia 8

--- Lectia8/Loops.java
+++ Lectia8/Loops.java
@@ -5,5 +5,19 @@ public class Loops{
             System.out.println(i);
             i++;
         }
+    /*    int count1 = 1;
+        while (count1 <=10){
+            int count2 = 1;
+            while (count2 <= 20){
+                System.out.println(count1+ " +" +count2);
+                count2++;
+            }
+            count1++;
+        }*/
+        System.out.println("Ciclul cu ajutorul for:");
+        for (int j = 0; j<5; j++){
+            System.out.println(j);
+        }
     }
+
 }
