e013cf60881d329e8aa70eefa0c4904e28239ff6
Repesco Vadim <repescov@gmail.com>
2020-08-19 19:23:26 +0300

    Conditionale lectia 8

--- Lectia8/Conditionale.java
+++ Lectia8/Conditionale.java
@@ -1,3 +1,4 @@
+
 public class Conditionale{
     public static void main(String[] params){
         int a1 = 10;
@@ -5,8 +6,39 @@ public class Conditionale{
         if (a1<a2) {
             System.out.println("Numarul "+a1+" este mai mic decat "+a2);
         }
+        int count = 6;
+        if (count > 5 & ++count<7){
+            ++count;
+        }
+        System.out.println("Contorul este la valoarea "+count);
+        int time = 20;
+        String result = (time < 18) ? "Buna ziua" : "Buna seara";
+        System.out.println(result);
+        int ziua = 3;
+        switch (ziua){
+            case 1:
+                System.out.println("Azi este luni");
+                break;
+            case 2:
+                System.out.println("Azi este marti");
+                break;
+            case 3:
+                System.out.println("Azi este miercuri");
+                break;
+            case 4:
+                System.out.println("Azi este joi");
+                break;
+            case 5:
+                System.out.println("Azi este vineri");
+                break;
+            case 6:
+                System.out.println("Azi este sambata");
+                break;
+            case 7:
+                System.out.println("Azi este duminica");
+                break;
+        }
 
     }
-
 
 }
