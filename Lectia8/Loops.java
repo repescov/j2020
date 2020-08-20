3ce497c4c4e08178dcd96f9160d8bb121f4f0d84
Repesco Vadim <repescov@gmail.com>
2020-08-20 19:02:47 +0300

    Break continue lectia 8

--- Lectia8/Loops.java
+++ Lectia8/Loops.java
@@ -1,9 +1,9 @@
 public class Loops{
     public static void main(String[] params){
-        int i = 0;
-        while (i < 5){
-            System.out.println(i);
-            i++;
+        int ii = 0;
+        while (ii < 5){
+            System.out.println(ii);
+            ii++;
         }
     /*    int count1 = 1;
         while (count1 <=10){
@@ -17,7 +17,21 @@ public class Loops{
         System.out.println("Ciclul cu ajutorul for:");
         for (int j = 0; j<5; j++){
             System.out.println(j);
+            if ( j==2 ) {
+                break; // Se va intrerupe ciclul la pasul j = 2
+            }
+        }
+        String[] cars = {"Ford","BMW","NISSAN","VAZ"};
+        for (String k:cars){
+            System.out.println(k);
+        }
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+            if (i == 9) {
+                break;
+            }
         }
     }
-
-}
+    }
