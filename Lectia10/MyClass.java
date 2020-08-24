76f9167f6abc20446588155177cc0a2596fea028
Repesco Vadim <repescov@gmail.com>
2020-08-24 18:43:38 +0300

    Lectia 10, folosirea package java.util, clasa Scanner

---
+++ Lectia10/MyClass.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class MyClass {
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Introdu numele");
+        String userName = myObj.nextLine();
+        System.out.println("Numele este "+userName);
+    }
+}
