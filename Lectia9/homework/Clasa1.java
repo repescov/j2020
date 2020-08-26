f2963bef4f77cde63f00e1e2734e01312ff84a15
Repesco Vadim <repescov@gmail.com>
2020-08-26 15:04:32 +0300

    Lectia 9, homework

---
+++ Lectia9/homework/Clasa1.java
@@ -0,0 +1,15 @@
+public class Clasa1 {
+    public void metodaIntreg(int numar){
+        System.out.println("Numarul intreg este "+numar);
+    }
+    public void metoda2String(String s1, String s2){
+
+    }
+    public void metoda0String(){
+        System.out.println("A fost apelata metoda string fara parametri");
+    }
+    public void metodaChar(char ch1){
+        int a=ch1;
+        System.out.println("Numarul a fost convertit in "+a);
+    }
+}
---
+++ Lectia9/homework/ClasaMain.java
@@ -0,0 +1,9 @@
+public class ClasaMain{
+    public static void main(String[] args) {
+        Clasa1 obiectulMeu = new Clasa1();
+        obiectulMeu.metodaIntreg(5);
+        obiectulMeu.metoda2String("primul string","al doilea string");
+        obiectulMeu.metoda0String();
+        obiectulMeu.metodaChar('1');
+    }
+}
