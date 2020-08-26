b9544f9f1bb7dbdd2a8d4525f8e003fb7bb02ecd
Repesco Vadim <repescov@gmail.com>
2020-08-26 16:06:12 +0300

    Lectia 10, homework

--- Lectia10/homework/Pachet1.java
+++ Lectia10/homework/Pachet1.java
@@ -9,8 +9,24 @@ public class Pachet1 {
     private void metodaPrivatePachet1(){
         System.out.println("Acesta este pachetul 1, metoda private");
     }
+    private void metodaPrivate2Pachet1(){
+        System.out.println("Acesta este pachetul 1, metoda private, a doua metoda");
+    }
+    public void apelareMetodePrivate(){
+        Pachet1 b = new Pachet1();
+        b.metodaPrivatePachet1();
+        Pachet1 b2 = new Pachet1();
+        b2.metodaDefaultPachet1();
+    }
 
     public static void main(String[] args) {
         System.out.println("Acesta este pachetul1, main");
+        System.out.println("Acum apelam metoda privata in cadrul clase");
+        Pachet1 a = new Pachet1();
+        a.metodaPrivatePachet1();
+        System.out.println("Acum apelam metoda private si default prin metoda publica in cadrul clasei");
+        Pachet1 c = new Pachet1();
+        c.apelareMetodePrivate();
     }
+
 }
--- Lectia10/homework/Pachet3.java
+++ Lectia10/homework/Pachet3.java
@@ -13,10 +13,19 @@ public class Pachet3 {
         System.out.println("Acesta este pachetul 3, metoda private");
     }
     public static void main(String[] args) {
-        System.out.println("Acesta este pachetul3, main");
+        System.out.println("--------------Acesta este pachetul3, main--------------");
+        System.out.println("--------------Aratam ca putem apela orice metoda din Pachet3--------------");
+        Pachet3 a1 = new Pachet3();
+        a1.metodaPublicaPachet3();
+        a1.metodaDefaultPachet3();
+        a1.metodaPrivatePachet3();
+        System.out.println("--------------Apelam la metoda publica din alt pachet--------------");
         Pachet1 a = new Pachet1();
         a.metodaPublicaPachet1();
 //        a.metodaDefaultPachet1();  eroare metodaDefaultPachet1() is not public in Pachet1; cannot be accessed from outside package
 //        a.metodaPrivatePachet1();  eroare metodaPrivatePachet1() has private access in Pachet1
+        System.out.println("--------------Acum apelam metoda privata si default prin metoda publica din alt pachet--------------");
+        Pachet1 c = new Pachet1();
+        c.apelareMetodePrivate();
     }
 }
