bb0583f636548d30fcce6dac9e071eba8c78ed74
Repesco Vadim <repescov@gmail.com>
2020-08-26 20:08:01 +0300

    Lectia 11, lucru cu String

--- Lectia11/SimpleStrings.java
+++ Lectia11/SimpleStrings.java
@@ -1,6 +1,18 @@
 public class SimpleStrings {
+    public static String s1;
+    public static String s2;
     public static void main(String[] args) {
         String text="Acesta este un simplu text";
         System.out.println(text);
+        s1 = "Text 1";
+        s2 = "Text 2";
+        System.out.println(s1+s2);
+        String st1 = "Text 1 ";
+        String st2 = " Text 2 ";
+        System.out.println(st1+st2);
+        String t1 = new String("Vasile");
+        String t2 = t1.concat("Bolea");
+        System.out.println(t1);
+        System.out.println(t2);
     }
 }
