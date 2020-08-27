ac205043dee02853228109949090d76978f26413
Repesco Vadim <repescov@gmail.com>
2020-08-27 19:17:34 +0300

    Lectia 12, exemple overload suma

--- Lectia12/Overload.java
+++ Lectia12/Overload.java
@@ -1,8 +1,17 @@
 package Lectia12;
 
 public class Overload {
+    public int suma(int x, int y){
+        return x+y;
+    }
+    public int suma(int x, int y, int z){
+        return x+y+z;
+    }
 
     public static void main(String[] args) {
         System.out.println("Programul a fost initializat");
+        Overload ref = new Overload();
+        System.out.println("Suma numerelor 2 si 15 este "+ref.suma(2,15));
+        System.out.println("Suma numerelor 2, 13 si 15 este "+ref.suma(2,13,15));
     }
 }
